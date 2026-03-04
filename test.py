687de3dc58629eb84441149dea1b4f0f3b6822ab
StuartSul <ssstuartss@gmail.com>
2026-03-03 20:24:38 -0800

    temporary test script

---
+++ test.py
@@ -0,0 +1,20 @@
+import torch
+torch.manual_seed(42)
+
+import megakittens
+
+
+@megakittens.compile(debug=True)
+def mlp(x: torch.Tensor, W: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    z = torch.matmul(x, W) + b
+    return torch.relu(z)
+
+
+if __name__ == "__main__":
+    M, N, K = 4, 8, 16
+    x = torch.rand(M, N)
+    W = torch.rand(N, K)
+    b = torch.rand(K)
+
+    print(f"M={M} N={N} K={K}")
+    print(mlp(x, W, b))
