12839098585aaf23ae16dc1e5cbfb218586fe92a
StuartSul <ssstuartss@gmail.com>
2026-03-03 20:45:48 -0800

    clean

--- megakittens/backend.py
+++ megakittens/backend.py
@@ -1,5 +1,18 @@
+from typing import Any, Callable, List
+
 import torch
 
 
-def _megakittens_backend(gm: torch.fx.GraphModule, example_inputs: list):
-    return gm.forward
+def megakittens_backend(
+    fn: Callable[..., Any],
+    *,
+    verify: bool = False,
+    profile: bool = False,
+    debug: bool = False,
+) -> Callable[[torch.fx.GraphModule, List[Any]], Callable[..., Any]]:
+    def _megakittens_backend(gm: torch.fx.GraphModule, example_inputs: List[Any]) -> Callable[..., Any]:
+        if debug:
+            print(f"[MegaKittens] Compiling function `{fn.__qualname__}`")
+        return gm.forward
+
+    return _megakittens_backend
--- megakittens/interface.py
+++ megakittens/interface.py
@@ -1,30 +1,34 @@
 import functools
+from typing import Any, Callable
 
 import torch
 
-from .backend import _megakittens_backend
+from .backend import megakittens_backend
 
 
 def compile(
-    fn=None, 
+    fn: Callable[..., Any] | None = None,
     *,
-    enable=True,
-    verify=False,
-    profile=False,
-    debug=False,
-):
-    """Compile a PyTorch function into a megakernel."""
-    def _compile(_fn):
-        if debug:
-            print(f"[MegaKittens] Compiling function `{_fn.__qualname__}`")
-        megakernel = torch._dynamo.optimize(
-            backend=_megakittens_backend,
+    enable: bool = True,
+    verify: bool = False,
+    profile: bool = False,
+    debug: bool = False,
+) -> Callable[..., Any]:
+    """Compile a PyTorch function into a MegaKernel."""
+    def _compile(_fn: Callable[..., Any]) -> Callable[..., Any]:
+        megakernel_fn = torch._dynamo.optimize(
+            backend=megakittens_backend(
+                fn=_fn,
+                verify=verify,
+                profile=profile,
+                debug=debug 
+            ),
             nopython=True, # graph breaks currently not supported (TODO: support it)
             disable=not enable,
             dynamic=False, # dynamic shapes not supported (TODO: support it)
         )(_fn)
-        functools.update_wrapper(megakernel, _fn)
-        return megakernel
+        functools.update_wrapper(megakernel_fn, _fn)
+        return megakernel_fn
     if fn is not None:
         return _compile(fn)
     return _compile
