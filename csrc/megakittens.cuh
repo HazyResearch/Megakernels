0aff9f558cdfdf76b69f4b6a8cc478ab5a67d6ee
Stuart Sul <ssstuartss@gmail.com>
2026-03-19 03:44:51 +0000

    clean again

--- csrc/megakittens.cuh
+++ csrc/megakittens.cuh
@@ -3,19 +3,15 @@
 #include "kittens.cuh"
 
 #include "schema.cuh"
-// #include "controller.cuh"
-// #include "workers.cuh"
-// #include "ops/ops.cuh"
+#include "ops/ops.cuh"
+#include "controller.cuh"
+#include "workers.cuh"
 
 namespace megakittens {
 
-template <typename Config, typename Globals, typename ...Ops>
+template <typename Config, typename Globals, typename>
 __global__ __launch_bounds__(Config::NUM_THREADS, Config::MIN_BLOCKS_PER_SM)
 void kernel(const __grid_constant__ Globals g) {
-
-    using C = Config;
-    using G = Globals;
-
     // Allocate shared memory
     __shared__ alignas(128) instruction_state_t<Config> instruction_states[Config::INSTRUCTION_PIPE_STAGES];
     __shared__ kittens::clc::handle clc_handle[Config::INSTRUCTION_PIPE_STAGES];
@@ -25,51 +21,50 @@ void kernel(const __grid_constant__ Globals g) {
     __shared__ kittens::semaphore page_finished[Config::NUM_PAGES];
     __shared__ kittens::semaphore tensor_finished;
     extern __shared__ int __shm[];
-    page_t<Config> (&pages)[Config::NUM_PAGES] = *reinterpret_cast<page_t<Config> (*)[Config::NUM_PAGES]>(
+    page_t<Config> (&pages)[Config::NUM_PAGES] = *reinterpret_cast<page_t<Config>(*)[Config::NUM_PAGES]>(
             reinterpret_cast<void *>(((uint64_t)&__shm[0] + 1023) & ~(uint64_t)1023));
 
     // Allocate tensor memory
     typename state<Config>::tensor_allocator_t tensor_alloc;
 
     // Instantiate MegaKittens state
-    state<Config> s{0, 0, clc_handle, clc_arrived, instruction_states, 
-                    instruction_arrived, instruction_finished,
-                    pages, page_finished,
-                    tensor_finished, tensor_alloc};
+    state<Config> s{0, 0, clc_handle, clc_arrived, instruction_states, instruction_arrived, instruction_finished,
+                    pages, page_finished, tensor_finished, tensor_alloc};
 
-    // Initialize all semaphores
-    if (threadIdx.x == 0) {
-        init_semaphore(tensor_finished, Config::NUM_CONSUMER_WARPS);
-        arrive(tensor_finished, Config::NUM_CONSUMER_WARPS); // flip the phasebit
-    }
+    // Initialize common semaphores
     if (threadIdx.x < Config::INSTRUCTION_PIPE_STAGES) {
         init_semaphore(instruction_arrived[threadIdx.x], 1);
-        init_semaphore(instruction_finished[threadIdx.x], Config::NUM_WARPS - 1);
-    }
-    if (threadIdx.x < Config::NUM_PAGES) {
-        init_semaphore(page_finished[threadIdx.x], Config::NUM_CONSUMER_WARPS);
-        arrive(page_finished[threadIdx.x], Config::NUM_CONSUMER_WARPS); // flip the phasebit
+    } else if (threadIdx.x < Config::INSTRUCTION_PIPE_STAGES*2) {
+        init_semaphore(instruction_finished[threadIdx.x - Config::INSTRUCTION_PIPE_STAGES], Config::NUM_WARPS - 1);
+    } else if (threadIdx.x < Config::INSTRUCTION_PIPE_STAGES*3) {
+        init_semaphore(clc_arrived[threadIdx.x - Config::INSTRUCTION_PIPE_STAGES*2], 1);
+    } else if (threadIdx.x < Config::INSTRUCTION_PIPE_STAGES*3 + Config::NUM_PAGES) {
+        init_semaphore(page_finished[threadIdx.x - Config::INSTRUCTION_PIPE_STAGES*3], Config::NUM_CONSUMER_WARPS);
+        arrive(page_finished[threadIdx.x - Config::INSTRUCTION_PIPE_STAGES*3], Config::NUM_CONSUMER_WARPS);
+    } else if (threadIdx.x < Config::INSTRUCTION_PIPE_STAGES*3 + Config::NUM_PAGES + 1) {
+        init_semaphore(tensor_finished, Config::NUM_CONSUMER_WARPS);
+        arrive(tensor_finished, Config::NUM_CONSUMER_WARPS);
     }
     kittens::everyone::tma::cluster::sync();
 
     // Initiate the main loops
     if (kittens::warpid() < Config::NUM_CONSUMER_WARPS) {
         kittens::warpgroup::increase_registers<Config::CONSUMER_REGISTERS>();
-        consumer::main_loop<Config, Globals, Ops...>(g, s);
+        consumer_loop<Config, Globals>(g, s);
     } else {
         kittens::warpgroup::decrease_registers<Config::NON_CONSUMER_REGISTERS>();
         switch (kittens::warpgroup::warpid()) {
             case 0:
-                controller_loop<Config, Globals, Ops...>(g, s);
+                controller_loop<Config, Globals>(g, s);
                 break;
             case 1:
-                loader::main_loop<Config, Globals, Ops...>(g, s);
+                loader_loop<Config, Globals>(g, s);
                 break;
             case 2:
-                launcher::main_loop<Config, Globals, Ops...>(g, s);
+                launcher_loop<Config, Globals>(g, s);
                 break;
             case 3:
-                storer::main_loop<Config, Globals, Ops...>(g, s);
+                storer_loop<Config, Globals>(g, s);
                 break;
             default:
                 asm volatile("{trap;\n}");
--- csrc/schema.cuh
+++ csrc/schema.cuh
@@ -6,62 +6,51 @@ namespace megakittens {
 
 struct default_config {
     static constexpr int INSTRUCTION_PIPE_STAGES = 2; // should not change
-    static constexpr int INSTRUCTION_WIDTH = 32;      // 128 bytes per instruction
+    static constexpr int INSTRUCTION_WIDTH = 32; // 128 bytes per instruction
     using instruction_t = int[INSTRUCTION_WIDTH];
     static_assert(INSTRUCTION_WIDTH <= 32); // for warp parallel processing
+
+    static constexpr int NUM_CONSUMER_WARPS = 8;
+    static constexpr int NUM_WARPS = 4 + NUM_CONSUMER_WARPS;
+    static constexpr int NUM_THREADS = NUM_WARPS * kittens::WARP_THREADS;
+    static constexpr int CONSUMER_REGISTERS = 104;
+    static constexpr int NON_CONSUMER_REGISTERS = 64;
 
     static constexpr int DYNAMIC_SEMAPHORES = 32;
     static_assert(DYNAMIC_SEMAPHORES <= 32); // for warp parallel processing
 
-    static constexpr int NUM_CONSUMER_WARPS = 16;
-    static constexpr int NUM_WARPS = 4 + NUM_CONSUMER_WARPS;
-    static constexpr int NUM_THREADS = NUM_WARPS * ::kittens::WARP_THREADS;
-    static constexpr int NUM_BLOCKS = 1;
-    static constexpr int CLUSTER_BLOCKS = 1;
-    static constexpr int MAX_SHARED_MEMORY = ::kittens::MAX_SHARED_MEMORY;
-
-    // Shared memory declared statically
     static constexpr int SCRATCH_BYTES = 4096;
-    static constexpr int STATIC_SHARED_MEMORY =
-        512 + INSTRUCTION_PIPELINE_STAGES *
-                  (SCRATCH_BYTES + (INSTRUCTION_WIDTH + TIMING_WIDTH) * 4 +
-                   DYNAMIC_SEMAPHORES * 8);
+    static constexpr int STATIC_SHARED_MEMORY = 256 + INSTRUCTION_PIPELINE_STAGES *
+        (INSTRUCTION_WIDTH*4 + 128 + DYNAMIC_SEMAPHORES*8 + SCRATCH_BYTES); // sizeof(instruction_state_t)
     static constexpr int DYNAMIC_SHARED_MEMORY = kittens::MAX_SHARED_MEMORY - STATIC_SHARED_MEMORY;
 
-    // Shared memory declared dynamically
     static constexpr int PAGE_SIZE = 16384;
     static constexpr int NUM_PAGES = DYNAMIC_SHARED_MEMORY / PAGE_SIZE;
-    static_assert(NUM_PAGES == 13, "NUM_PAGES must be 13");
     static_assert(NUM_PAGES <= 32); // for warp parallel processing
 
-    static constexpr bool TIMING_RECORD_ENABLED = false;
-
-    static constexpr bool GMEM_SPIN_LOOP_SLEEP_NANOS = 20;
-
-    static constexpr int CONSUMER_REGISTERS = 104;
-    static constexpr int NON_CONSUMER_REGISTERS = 64;
+    static constexpr bool SPIN_LOOP_SLEEP_NS = 20;
 };
 
 template <typename Config>
-using instruction_layout = kittens::gl<int, 1, 1, -1, Config::INSTRUCTION_WIDTH>;
+using instruction_gl = kittens::gl<int, 1, 1, -1, Config::INSTRUCTION_WIDTH>;
 
 template <typename Config> 
 struct __align__(128) instruction_state_t {
     Config::instruction_t instruction;
     int pid_order[Config::NUM_PAGES];
-    int _padding[((Config::NUM_PAGES + 31) & ~31) - Config::NUM_PAGES]; // round up to multiple of 32
+    int _padding[((Config::NUM_PAGES + 31) & ~31) - Config::NUM_PAGES]; // make pid_order + _padding 128 bytes
     kittens::semaphore semaphores[Config::DYNAMIC_SEMAPHORES];
-    int scratch[Config::SCRATCH_BYTES / 4]; // todo keep it really /4?
+    int scratch[Config::SCRATCH_BYTES/sizeof(int)];
 };
 
 template <typename Config>
 struct page_t {
-    int data[Config::PAGE_SIZE / sizeof(int)];
+    int data[Config::PAGE_SIZE/sizeof(int)];
     __device__ inline void *ptr(int byte_offset = 0) {
-        return (void *)(data + byte_offset / sizeof(int));
+        return reinterpret_cast<void *>(reinterpret_cast<uint64_t>(&data[0])+byte_offset);
     }
     __device__ inline const void *ptr(int byte_offset = 0) const {
-        return (const void *)(data + byte_offset / sizeof(int));
+        return reinterpret_cast<const void *>(reinterpret_cast<uint64_t>(&data[0])+byte_offset);
     }
 };
 
@@ -102,18 +91,14 @@ struct state_t {
         return instruction_states[stage].semaphores;
     }
     __device__ inline void *scratch() const {
-        return (void *)&instruction_states[stage].scratch[0];
+        return reinterpret_cast<void *>(&instruction_states[stage].scratch[0]);
     }
 
     __device__ inline int pid(int lid) {
         return pid_order()[lid];
     }
     __device__ inline void finish_page(int pid, int count) {
-        arrive(page_finished[pid], count);
-    }
-    __device__ inline void warp_finish_page(int pid, int count) {
-        if (kittens::warp::laneid() == 0)
-            finish_page(pid, count);
+        kittens::arrive(page_finished[pid], count);
     }
     __device__ inline void wait_page_ready(int pid) {
         kittens::wait(page_finished[pid], iter & 0b1);
