a99396dbb1bbd1727ea58c8b0c101984d819f6ff
Stuart Sul <ssstuartss@gmail.com>
2026-04-07 22:57:01 +0000

    Auto import

--- megakittens/itypes/__init__.py
+++ megakittens/itypes/__init__.py
@@ -1,1 +1,5 @@
-from . import add, attention, causal_attention, gemm, relu, rmsnorm
+from importlib import import_module
+from pkgutil import iter_modules
+
+for _info in iter_modules(__path__):
+    import_module(f".{_info.name}", __name__)
