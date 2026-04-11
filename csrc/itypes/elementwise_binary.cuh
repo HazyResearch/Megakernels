15c93906da3133ad161d08962cd9a72fddf19851
Stuart Sul <ssstuartss@gmail.com>
2026-04-11 20:33:04 +0000

    Clean up elementwise binary kernel

--- csrc/itypes/elementwise_binary.cuh
+++ csrc/itypes/elementwise_binary.cuh
@@ -46,16 +46,15 @@ struct BinaryOpList {
 // Tensor indices: first N are sources, last one is DST (matches {tensors} convention)
 template <typename Config, typename Globals, typename OpList, int... TensorIndices>
 struct ElementwiseBinary {
-    static constexpr int N_OPS = OpList::size;
+    static constexpr int NUM_OPS = OpList::size;
     static constexpr int N_TENSORS = sizeof...(TensorIndices);
-    static constexpr int N_INPUTS = N_OPS + 1;
-    static_assert(N_TENSORS == N_INPUTS + 1, "Need N+1 tensor indices for N ops (N inputs + 1 output)");
+    static constexpr int NUM_INPUTS = NUM_OPS + 1;
+    static_assert(N_TENSORS == NUM_INPUTS + 1, "Need N+1 tensor indices for N ops (N inputs + 1 output)");
 
     static constexpr int DST = last_int<TensorIndices...>::value;
-    static constexpr int PAGES_PER_TILE = N_INPUTS;
-    static constexpr int TILES_PER_INST = Config::NUM_PAGES / PAGES_PER_TILE;
+    static constexpr int TILES_PER_INST = Config::NUM_PAGES / NUM_INPUTS;
     static_assert(TILES_PER_INST >= 1, "Not enough pages for this many inputs");
-    static constexpr int NUM_USED_PAGES = TILES_PER_INST * PAGES_PER_TILE;
+    static constexpr int NUM_USED_PAGES = TILES_PER_INST * NUM_INPUTS;
 
     using tile_t = kittens::st<kittens::bf16, 128, 128>;
 
@@ -64,38 +63,35 @@ struct ElementwiseBinary {
     struct controller {
         __device__ __forceinline__ static int lid_release_order(const Globals &g, state_t<Config> &s, int query) {
             const int num_tiles = s.instruction().indices[4];
-            const int num_unused = Config::NUM_PAGES - num_tiles * PAGES_PER_TILE;
+            const int num_unused = Config::NUM_PAGES - num_tiles*NUM_INPUTS;
             if (query < num_unused)
-                return num_tiles * PAGES_PER_TILE + query;
+                return num_tiles * NUM_INPUTS + query;
             const int used_query = query - num_unused;
             // Release in reverse input order: last input first, first input (used for store) last
-            const int input_group = (PAGES_PER_TILE - 1) - used_query / num_tiles;
+            const int input_group = (NUM_INPUTS - 1) - used_query / num_tiles;
             const int tile_idx = used_query % num_tiles;
-            return tile_idx * PAGES_PER_TILE + input_group;
+            return tile_idx * NUM_INPUTS + input_group;
         }
         __device__ __forceinline__ static int init_semaphores(const Globals &g, state_t<Config> &s) {
-            if (kittens::laneid() < TILES_PER_INST)
+            const int num_tiles = s.instruction().indices[4];
+            if (kittens::laneid() < num_tiles)
                 kittens::init_semaphore(inputs_arrived(s, kittens::laneid()), 1);
-            return TILES_PER_INST;
+            return num_tiles;
         }
     };
 
     struct loader {
         template <int I>
-        __device__ __forceinline__ static void load_one(const Globals &g, state_t<Config> &s,
-                                                        int tile_idx, int batch, int depth, int tile_row, int tile_col) {
-            const int pid = s.lid_to_pid(tile_idx * PAGES_PER_TILE + I);
+        __device__ __forceinline__ static void load_tile(const Globals &g, state_t<Config> &s, int tile_idx, int batch, int depth, int tile_row, int tile_col) {
+            const int pid = s.lid_to_pid(tile_idx*NUM_INPUTS + I);
             s.page_wait(pid);
-            tile_t &st = s.pages[pid].template as<tile_t>();
-            kittens::tma::load_async(st, g.template gls<nth_int<I, TensorIndices...>::value>(),
-                                     {batch, depth, tile_row, tile_col}, inputs_arrived(s, tile_idx));
+            tile_t &tile = s.pages[pid].template as<tile_t>();
+            kittens::tma::load_async(tile, g.template gls<nth_int<I, TensorIndices...>::value>(), {batch, depth, tile_row, tile_col}, inputs_arrived(s, tile_idx));
         }
 
         template <int... Is>
-        __device__ __forceinline__ static void load_all(const Globals &g, state_t<Config> &s,
-                                                        int tile_idx, int batch, int depth, int tile_row, int tile_col,
-                                                        std::integer_sequence<int, Is...>) {
-            (load_one<Is>(g, s, tile_idx, batch, depth, tile_row, tile_col), ...);
+        __device__ __forceinline__ static void load_all_tiles(const Globals &g, state_t<Config> &s, int tile_idx, int batch, int depth, int tile_row, int tile_col, std::integer_sequence<int, Is...>) {
+            (load_tile<Is>(g, s, tile_idx, batch, depth, tile_row, tile_col), ...);
         }
 
         __device__ __forceinline__ static void run(const Globals &g, state_t<Config> &s) {
@@ -108,13 +104,12 @@ struct ElementwiseBinary {
 
             if (kittens::warp::elect_leader()) {
                 all_input_barrier_wait<Config>(g, instruction);
-
-                for (int i = 0; i < num_tiles; i++) {
-                    kittens::tma::expect_bytes(inputs_arrived(s, i), N_INPUTS * sizeof(tile_t));
-                    load_all(g, s, i, batch, depth, tile_row, tile_col_start + i, std::make_integer_sequence<int, N_INPUTS>{});
+                for (int tile_idx = 0; tile_idx < num_tiles; tile_idx++) {
+                    kittens::tma::expect_bytes(inputs_arrived(s, tile_idx), NUM_INPUTS*sizeof(tile_t)); // TODO: try optimizing with more fine-grained mbarrier arrivals
+                    load_all_tiles(g, s, tile_idx, batch, depth, tile_row, tile_col_start + tile_idx, std::make_integer_sequence<int, NUM_INPUTS>{});
                 }
             } else if (kittens::warp::elect_leader_from_active()) {
-                for (int i = num_tiles * PAGES_PER_TILE; i < Config::NUM_PAGES; i++) {
+                for (int i = num_tiles*NUM_INPUTS; i < Config::NUM_PAGES; i++) {
                     s.page_wait(s.lid_to_pid(i));
                     s.page_finish(s.lid_to_pid(i));
                 }
@@ -131,16 +126,11 @@ struct ElementwiseBinary {
 
     struct consumer {
         using consumer_group = kittens::group<Config::NUM_CONSUMER_WARPS>;
-        using reg_t = kittens::rt_bf<16, 128>;
+        using reg_tile_t = kittens::rt_bf<16, 128>;
 
-        template <int OpIdx>
-        __device__ __forceinline__ static void apply_one(reg_t &acc, reg_t *regs) {
-            apply_binary_op<OpList::template get<OpIdx>(), consumer_group>(acc, acc, regs[OpIdx + 1]);
-        }
-
-        template <int... OpIs>
-        __device__ __forceinline__ static void apply_remaining(reg_t &acc, reg_t *regs, std::integer_sequence<int, OpIs...>) {
-            (apply_one<OpIs + 1>(acc, regs), ...); // +1 because ops[0] already applied
+        template <int... Is>
+        __device__ __forceinline__ static void apply_all_binary_ops(reg_tile_t *reg_tiles, std::integer_sequence<int, Is...>) {
+            (apply_binary_op<OpList::template get<Is>(), consumer_group>(reg_tiles[0], reg_tiles[0], reg_tiles[Is + 1]), ...);
         }
 
         __device__ __forceinline__ static void run(const Globals &g, state_t<Config> &s) {
@@ -152,38 +142,34 @@ struct ElementwiseBinary {
             const int tile_col_start = instruction.indices[3];
             const int num_tiles = instruction.indices[4];
 
-            for (int t = 0; t < num_tiles; t++) {
-                kittens::wait(inputs_arrived(s, t), 0);
+            for (int tile_idx = 0; tile_idx < num_tiles; tile_idx++) {
+                kittens::wait(inputs_arrived(s, tile_idx), 0);
 
-                reg_t regs[N_INPUTS];
+                reg_tile_t reg_tiles[NUM_INPUTS];
                 #pragma unroll
-                for (int i = 0; i < N_INPUTS; i++) {
-                    tile_t &st = s.pages[s.lid_to_pid(t * PAGES_PER_TILE + i)].template as<tile_t>();
-                    consumer_group::load(regs[i], st);
+                for (int i = 0; i < NUM_INPUTS; i++) {
+                    tile_t &tile = s.pages[s.lid_to_pid(tile_idx*NUM_INPUTS + i)].template as<tile_t>();
+                    consumer_group::load(reg_tiles[i], tile);
                 }
 
-                // acc = Op0(src0, src1), then acc = OpI(acc, src[I+1]) for remaining
-                apply_binary_op<OpList::template get<0>(), consumer_group>(regs[0], regs[0], regs[1]);
-                if constexpr (N_OPS > 1) {
-                    apply_remaining(regs[0], regs, std::make_integer_sequence<int, N_OPS - 1>{});
-                }
+                apply_all_binary_ops(reg_tiles, std::make_integer_sequence<int, NUM_OPS>{});
 
-                tile_t &dst_st = s.pages[s.lid_to_pid(t * PAGES_PER_TILE)].template as<tile_t>();
-                consumer_group::store(dst_st, regs[0]);
+                tile_t &dst_tile = s.pages[s.lid_to_pid(tile_idx*NUM_INPUTS)].template as<tile_t>();
+                consumer_group::store(dst_tile, reg_tiles[0]);
                 consumer_group::sync(1);
 
                 if (consumer_group::elect_leader()) {
                     #pragma unroll
-                    for (int i = N_INPUTS - 1; i >= 1; i--)
-                        s.page_finish(s.lid_to_pid(t * PAGES_PER_TILE + i));
-                    if (t == 0) all_reuse_barrier_wait<Config>(g, instruction);
-                    kittens::tma::store_async(dst_gl, dst_st, {batch, depth, tile_row, tile_col_start + t});
+                    for (int i = NUM_INPUTS - 1; i >= 1; i--) // for cleaner lid_release_order logic
+                        s.page_finish(s.lid_to_pid(tile_idx*NUM_INPUTS + i));
+                    if (tile_idx == 0) all_reuse_barrier_wait<Config>(g, instruction);
+                    kittens::tma::store_async(dst_gl, dst_tile, {batch, depth, tile_row, tile_col_start + tile_idx});
                 }
             }
 
             if (consumer_group::elect_leader()) {
                 kittens::tma::store_async_wait();
-                for (int t = 0; t < num_tiles; t++) s.page_finish(s.lid_to_pid(t * PAGES_PER_TILE));
+                for (int tile_idx = 0; tile_idx < num_tiles; tile_idx++) s.page_finish(s.lid_to_pid(tile_idx*NUM_INPUTS));
                 all_barrier_arrive<Config>(g, instruction);
             }
         }
