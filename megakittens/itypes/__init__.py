97d365bd23a0dd9b02e21ad5ad1489f2720b4f38
Stuart Sul <ssstuartss@gmail.com>
2026-04-12 23:36:02 +0000

    Import submodules too

--- megakittens/itypes/__init__.py
+++ megakittens/itypes/__init__.py
@@ -1,5 +1,8 @@
 from importlib import import_module
 from pkgutil import iter_modules
 
-for _info in iter_modules(__path__):
-    import_module(f".{_info.name}", __name__)
+for module_info in iter_modules(__path__):
+    module = import_module(f".{module_info.name}", __name__)
+    if module_info.ispkg:  # is this a folder with __init__.py?
+        for sub_module_info in iter_modules(module.__path__):
+            import_module(f".{sub_module_info.name}", module.__name__)
