1103a79278a84d6374d9c54f5567747e44c80db9
nashbrown <nashb200@gmail.com>
2026-04-19 05:01:09 +0000

    optimize upgate auto schedule

--- megakittens/itypes/llama1b/rms_upgate_silu.py
+++ megakittens/itypes/llama1b/rms_upgate_silu.py
@@ -6,6 +6,7 @@ from ...schema.dtype import DType
 from ...schema.itype import IType
 from ...schema.tensor import TensorMeta, TensorRange, TensorSpec
 from ...jit.pykittens import sv, st
+from ...jit.cuda_utils import get_sm_count
 
 
 @torch.library.custom_op("megakittens::rms_upgate_silu", mutates_args=())
@@ -101,8 +102,8 @@ class RmsUpgateSilu(IType):
         src_ranges: Tuple[TensorRange, ...],
         dst_ranges: Tuple[TensorRange, ...],
     ) -> int:
-        out_range = dst_ranges[0]
-        return out_range[-1].size // 16
+        num_blocks = dst_ranges[0][-1].size // 16
+        return min(get_sm_count(), num_blocks)
 
     def block_indices(
         self,
@@ -111,12 +112,11 @@ class RmsUpgateSilu(IType):
         src_ranges: Tuple[TensorRange, ...],
         dst_ranges: Tuple[TensorRange, ...],
     ) -> List[Tuple[int, ...]]:
-        out_range = dst_ranges[0]
         layer_idx = src_ranges[2][-3].start
-        num_blocks = out_range[-1].size // 16
-        block_start = out_range[-1].start // 16
-        block_stop = out_range[-1].stop // 16
-        return [(layer_idx, b, num_blocks, num_blocks) for b in range(block_start, block_stop)]
+        num_blocks = dst_ranges[0][-1].size // 16
+        sm_count = get_sm_count()
+        n_insts = min(sm_count, num_blocks)
+        return [(layer_idx, sm, sm_count, num_blocks) for sm in range(n_insts)]
 
     def test_args(self, case):
         intermediate_dim, = case
@@ -137,9 +137,13 @@ class RmsUpgateSilu(IType):
         up_region = ((layer_idx, layer_idx + 1), (0, intermediate_dim), (0, n))
         gate_region = ((layer_idx, layer_idx + 1), (0, intermediate_dim), (0, n))
         eps_region = ((0, 1),)
-        # one block per inst under auto: tight out region for fine-grained barriers
-        out_region = ((sm_idx * 16, (sm_idx + 1) * 16),)
-        return [[x_region], [norm_region], [up_region], [gate_region], [eps_region]], [[out_region]]
+        # one box per distinct down-proj-sized sub-chunk this SM's stride hits
+        sub_tiles = n // 16
+        sub_rows = sub_tiles * 16
+        iters = (total_blocks - sm_idx + sm_count - 1) // sm_count
+        subs = sorted({(sm_idx + ii * sm_count) // sub_tiles for ii in range(iters)})
+        out_regions = [((s * sub_rows, (s + 1) * sub_rows),) for s in subs]
+        return [[x_region], [norm_region], [up_region], [gate_region], [eps_region]], [out_regions]
 
     def validate(
         self,
